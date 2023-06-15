3f015daea2fc7b2fbd2f03df20cd52b9565daa4f
namodynamic <nnamdi4u09@gmail.com>
2023-06-15 21:01:50 +0100

    Update

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,10 +2,13 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/Shoplify",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection error:", error.message);
