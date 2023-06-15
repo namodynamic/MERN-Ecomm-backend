f35000e1a78096f17babaee3b4c5dcbc37294e70
namodynamic <nnamdi4u09@gmail.com>
2023-06-15 19:12:34 +0100

    Update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import express from "express";
-// const express = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const dbConnect = require("./config/dbConnect");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
